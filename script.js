82f3dd1b131c1d39537e474653aabc07603f9497
Zoms <martinezoms15@gmail.com>
2022-01-18 17:42:26 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,71 @@
+const quoteContainer = document.getElementById("quote-container");
+const quoteText = document.getElementById("quote");
+const authorText = document.getElementById("author");
+const twitterBtn = document.getElementById("twitter");
+const newQuoteBtn = document.getElementById("new-quote");
+const loader = document.getElementById("loader");
+
+let apiQuotes = [];
+
+// Show loading
+function loading() {
+  loader.hidden = false;
+  quoteContainer.hidden = true;
+}
+
+// hide loading
+function complete() {
+  loader.hidden = true;
+  quoteContainer.hidden = false;
+}
+
+// Show new quote
+function newQuote() {
+  loading();
+  const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+  // check if author is empty and replace with unknown
+  if (!quote.author) {
+    authorText.textContent = "Unknown";
+  } else {
+    authorText.textContent = quote.author;
+  }
+  // check quote length to determine styling
+  if (quote.text.length > 120) {
+    quoteText.classList.add("long-quote");
+  } else {
+    quoteText.classList.remove("long-quote");
+  }
+  quoteText.textContent = quote.text;
+  // hide loader
+  complete();
+}
+
+// Get quotes from API
+async function getQuotes() {
+  loading();
+  const apiUrl = "https://type.fit/api/quotes";
+  try {
+    const response = await fetch(apiUrl);
+    apiQuotes = await response.json();
+    newQuote();
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+// Tweet a quote
+function tweetQuote() {
+  const tweetUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+  window.open(tweetUrl, "_blank");
+}
+
+// using time interval to change quotes
+setInterval(newQuote, 10000);
+
+// Event listener
+newQuoteBtn.addEventListener("click", newQuote);
+
+twitterBtn.addEventListener("click", tweetQuote);
+
+// fuction runs on page load
+getQuotes();
